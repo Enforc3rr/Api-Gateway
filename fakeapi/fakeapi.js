c400c2f0746a01689fb085757dfb39044fc8888c
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-11-22 23:39:02 +0530

    Added Fake API.

--- fakeapi/fakeapi.js
+++ fakeapi/fakeapi.js
@@ -9,6 +9,11 @@ app.get("/fakeapi",((req, res, next) => {
     return res.send("Hello From Fake API server");
 }));
 
+app.get("/fakeapi2",((req, res, next) => {
+    console.log("Fake API part 2");
+    return res.send("Hello From Fake server part 2");
+}));
+
 
 const PORT = process.env.PORT || 8001;
 app.listen(PORT , ()=>console.log(`Fake-API Server started on port ${PORT}`));
