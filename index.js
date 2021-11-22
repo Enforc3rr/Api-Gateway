1363850ae1d97110241e0f33d9bcf4b256bf6e31
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-11-22 23:39:48 +0530

    Made some changes on index.js and routes.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = require("./routes/routes");
 
-app.use(express.json);
+app.use(express.json());
 app.use("/",router);
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
-router.route("/:api",(req,res)=>{
-    console.log(req.params.api);
+router.all("/:api",(req,res)=>{
+    // console.log(req.params.api);
+    axios.get(`http://localhost:8001/${req.params.api}`)
+        .then(response =>{
+            console.log(response.data);
+            return res.send(response.data);
+        })
+        .catch(error=>{
+            return res.send(error);
+        });
 });
 
 module.exports = router;
